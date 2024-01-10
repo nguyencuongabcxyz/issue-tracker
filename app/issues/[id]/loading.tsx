abbd57356688f3e515b5ee1e0ad8a50fcbcd727a
SIOUXCHINA\Cuong.Nguyen <cuong.nguyen@sioux.eu>
2024-01-10 23:53:03 +0700

    Style issue detail page

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssueDetailLoading = () => {
+  return (
+    <div>
+      Loading...
+    </div>
+  )
+}
+
+export default IssueDetailLoading
---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,34 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  console.log(issue);
+
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex gap={"3"} my={"2"}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        {issue.description}
+      </Card>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
