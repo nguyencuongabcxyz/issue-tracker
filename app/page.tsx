fbee1783c819039a5db63ecd14cef8e5ec06e3e5
SIOUXCHINA\Cuong.Nguyen <cuong.nguyen@sioux.eu>
2024-01-23 23:34:10 +0700

    Add IssueChart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
-import LatestIssues from "./LatestIssues";
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -13,5 +13,5 @@ export default async function Home() {
   const inProgress = await prisma.issue.count({
     where: { status: "IN_PROGRESS" },
   });
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 }
